38b68891ca77d495b1392a5ae16cf1d70d79c43a
Theo Browne <me@t3.gg>
2022-12-05 21:26:11 -0800

    simplify

--- 6/b.ts
+++ 6/b.ts
@@ -5,16 +5,11 @@ const input = await Deno.readTextFile("./input.txt");
 console.log(input);
 
 const chars = input.split("");
-
 for (let i = 13; i < chars.length; i++) {
-  const char = chars[i];
-
-  const lastFourAreUnique = chars.slice(i - 14, i);
-
-  const asSet = new Set(lastFourAreUnique);
+  const asSet = new Set(chars.slice(i - 14, i));
 
   if (asSet.size === 14) {
-    console.log(char, i);
+    console.log(i);
     break;
   }
 }
