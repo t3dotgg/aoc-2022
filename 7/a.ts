d86b3e7d0d33108ed399c57389c3254e8a5da97a
Theo Browne <me@t3.gg>
2022-12-06 22:23:54 -0800

    Day 7 ouch

---
+++ 7/a.ts
@@ -0,0 +1,63 @@
+export const x = "";
+
+const input = (await Deno.readTextFile("./input.txt")).split("\n");
+// const input = (await Deno.readTextFile("./test.txt")).split("\n");
+console.log(input);
+
+type Dir = {
+  parent?: Dir;
+  files: { [name: string]: number };
+  dirs: { [name: string]: Dir };
+};
+
+const fs: Dir = { files: {}, dirs: {} };
+
+let current = fs;
+
+for (const command of input) {
+  const [cmd, ...rest] = command.split(" ");
+
+  if (cmd === "$") {
+    if (rest[0] === "cd") {
+      const dir = rest[1];
+      if (dir === "..") {
+        current = current.parent!;
+      } else if (dir === "/") {
+        current = fs;
+      } else {
+        if (!current.dirs[dir]) {
+          current.dirs[dir] = { parent: current, files: {}, dirs: {} };
+        }
+        current = current.dirs[dir];
+      }
+    }
+  } else if (cmd !== "dir") {
+    current.files[rest[0]] = parseInt(cmd);
+  }
+}
+
+const dirSizes: number[] = [];
+
+const computeFileSize = (curr: Dir): number => {
+  let size = 0;
+
+  for (const file in curr.files) {
+    size += curr.files[file];
+  }
+
+  for (const dir in curr.dirs) {
+    const dirSize = computeFileSize(curr.dirs[dir]);
+    size += dirSize;
+
+    dirSizes.push(dirSize);
+  }
+
+  return size;
+};
+
+computeFileSize(fs);
+
+const sumOfSub100k = dirSizes
+  .filter((x) => x <= 100000)
+  .reduce((a, b) => a + b);
+console.log("part 1:", sumOfSub100k);
