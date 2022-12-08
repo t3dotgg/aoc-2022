dfb86eab1c509cb9474c1004aedbcf5772de121a
Theo Browne <me@t3.gg>
2022-12-07 21:41:15 -0800

    flatten

--- 8/b.ts
+++ 8/b.ts
@@ -11,7 +11,6 @@ let bestScore = 0;
 
 const treeIsVisible = (x: number, y: number) => {
   const tree = numGrid[x][y];
-
   let a = 0;
   let b = 0;
   let c = 0;
@@ -19,36 +18,22 @@ const treeIsVisible = (x: number, y: number) => {
 
   for (let j = x - 1; j >= 0; j--) {
     a++;
-    if (numGrid[j][y] >= tree) {
-      break;
-    }
+    if (numGrid[j][y] >= tree) break;
   }
-
   for (let j = x + 1; j < numGrid.length; j++) {
     b++;
-    if (numGrid[j][y] >= tree) {
-      break;
-    }
+    if (numGrid[j][y] >= tree) break;
   }
-
   for (let k = y - 1; k >= 0; k--) {
     c++;
-    if (numGrid[x][k] >= tree) {
-      break;
-    }
+    if (numGrid[x][k] >= tree) break;
   }
-
   for (let k = y + 1; k < numGrid[x].length; k++) {
     d++;
-    if (numGrid[x][k] >= tree) {
-      break;
-    }
+    if (numGrid[x][k] >= tree) break;
   }
-
   const scenicScore = a * b * c * d;
-  if (scenicScore > bestScore) {
-    bestScore = scenicScore;
-  }
+  if (scenicScore > bestScore) bestScore = scenicScore;
 };
 
 for (let x = 0; x < numGrid.length; x++) {
