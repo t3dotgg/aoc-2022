7bd8b0e2411fd33a7be53877e0f55cf1952f6b4f
Theo Browne <me@t3.gg>
2022-12-09 21:40:13 -0800

    Cleanup part 2

--- 10/b.ts
+++ 10/b.ts
@@ -5,30 +5,16 @@ const input = await Deno.readTextFile("./input.txt");
 
 const lines = input.split("\n");
 
-console.log(lines);
-
 let x = 1;
-
-let instructions: number[] = [];
+const instructions: number[] = [];
 
 for (let i = 0; i < lines.length; i++) {
-  const l = lines[i];
+  instructions.push(0);
 
-  const [ins, amount] = l.split(" ");
-
-  if (ins === "noop") instructions.push(0);
+  const [ins, amount] = lines[i].split(" ");
   if (ins === "addx") {
-    instructions.push(0);
     instructions.push(parseInt(amount));
   }
-
-  // if ([20, 60, 100, 140, 180, 220].includes(i)) {
-  //   totals.push(x * (i + 1));
-  // }
-
-  // x += nextChange;
-  // nextChange = nextNextChange;
-  // nextNextChange = 0;
 }
 
 let rowOfFourtySpaces = "                                        ".split("");
@@ -42,17 +28,13 @@ let vertical = [
 ];
 
 instructions.forEach((ins, index) => {
-  console.log("signal at", index, "is", x);
   const [dx, dy] = [index % 40, Math.floor(index / 40)];
-
-  console.log(dy, dx);
 
   const shouldPaint = dx === x || dx === x - 1 || dx === x + 1;
   vertical[dy][dx] = shouldPaint ? "X" : " ";
   x += ins;
 });
 
-// print vertical as grid
 vertical.forEach((row) => {
   console.log(row.join(""));
 });
