e9ed6fccf41711a6f80a282f378ad6a259672646
Theo Browne <me@t3.gg>
2022-12-09 21:42:02 -0800

    More cleanup

--- 10/b.ts
+++ 10/b.ts
@@ -12,29 +12,22 @@ for (let i = 0; i < lines.length; i++) {
   instructions.push(0);
 
   const [ins, amount] = lines[i].split(" ");
-  if (ins === "addx") {
-    instructions.push(parseInt(amount));
-  }
+  if (ins === "addx") instructions.push(parseInt(amount));
 }
 
-let rowOfFourtySpaces = "                                        ".split("");
-let vertical = [
-  [...rowOfFourtySpaces],
-  [...rowOfFourtySpaces],
-  [...rowOfFourtySpaces],
-  [...rowOfFourtySpaces],
-  [...rowOfFourtySpaces],
-  [...rowOfFourtySpaces],
-];
+// define grid of 40x6
+const grid: string[][] = Array.from({ length: 6 }, () =>
+  new Array(40).fill(" ")
+);
 
 instructions.forEach((ins, index) => {
   const [dx, dy] = [index % 40, Math.floor(index / 40)];
 
   const shouldPaint = dx === x || dx === x - 1 || dx === x + 1;
-  vertical[dy][dx] = shouldPaint ? "X" : " ";
+  grid[dy][dx] = shouldPaint ? "X" : " ";
   x += ins;
 });
 
-vertical.forEach((row) => {
+grid.forEach((row) => {
   console.log(row.join(""));
 });
